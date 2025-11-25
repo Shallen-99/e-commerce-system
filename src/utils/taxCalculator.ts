bfbf636932109b49f8a4bf2ca3fda3b0ce0df1a3
Shallen-99 <shallenburks@gmail.com>
2025-11-25 15:59:14 -0500

    added calculateTax function

--- src/utils/taxCalculator.ts
+++ src/utils/taxCalculator.ts
@@ -1,3 +1,12 @@
 //Apply a default standard tax rate of 4.75% to each product; 
 // however, products with a category of “groceries” should only
 //  be taxed at 3%.
+import {Product} from "../models/Product";
+
+export function calculateTax(product: Product): any {
+    if (product.category === "groceries") {
+        product.price * 0.03
+    } else {
+        product.price * 0.0475
+    } 
+}
